34e0480282115503ac7e40e3c87d029d0d6ab968
Nabiel Alif <nabiel.alif01@gmail.com>
2022-04-01 02:12:22 +0700

    Add operator functions

---
+++ main.js
@@ -0,0 +1,10 @@
+//Operator functions
+const add = (a, b) => a + b
+const subtract = (a, b) => a - b
+const multiply = (a, b) => a * b
+const divide = (a, b) => a * b
+
+const operate = (operator, num1, num2) => {
+   num1 = Number(num1)
+   num2 = Number(num2)
+};
