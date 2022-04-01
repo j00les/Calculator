f0b85c28fe787e9e119c576478571336d0498f2a
Nabiel Alif <nabiel.alif01@gmail.com>
2022-04-01 18:30:06 +0700

    UI update

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
+const display  = document.querySelector('.display')
+
 //Operator functions
 const add = (a, b) => a + b
 const subtract = (a, b) => a - b
 const multiply = (a, b) => a * b
-const divide = (a, b) => a * b
+const divide = (a, b) => a / b
 
 const operate = (operator, num1, num2) => {
    num1 = Number(num1)
    num2 = Number(num2)
+   
+   if (operator === '+') {
+      return add(num1, num2)
+   
+   } else if (operator === '-') {
+      return subtract(num1, num2)
+   
+   } else if (operator === 'x') {
+      return multiply(num1, num2)
+   
+   }else if (operator === '/')  {
+      if (b === 0) return null
+      return divide(num1, num2)
+   };
 };
