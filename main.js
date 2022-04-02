cb939255dd0ea02f7e5c5ed66d417a0e93c27a12
Nabiel Alif <nabiel.alif01@gmail.com>
2022-04-02 17:20:28 +0700

    Reset screen function

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
+const topDisplay = document.querySelector('.top-display')
 const currentDisplay = document.querySelector('.current-display');
 const operandButtons = document.querySelectorAll('.operand');
 const operatorButtons = document.querySelectorAll('.operator');
+const clearBtn = document.querySelector('.cBtn')
+
+let firstOperand = ''
+let secondOperand = ''
+let doResetScreen = false
+let currentOperator = null
+
+clearBtn.addEventListener('click', clear)
+window.addEventListener('keydown', keyboardPress)
+
+operatorButtons.forEach(button => button.addEventListener('click', () => addOperator(button.textContent)));
+
+operandButtons.forEach(button => button.addEventListener('click', () => addOperand(button.textContent)));
 
 // Operator functions
 const add = (a, b) => a + b;
@@ -8,12 +22,6 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
-operandButtons.forEach(button => button.addEventListener('click', () => appendNumber(button.textContent)));
-
-
-function appendNumber(e) {
-   currentDisplay.textContent += e
-}
 
 const operate = (operator, num1, num2) => {
    num1 = Number(num1)
@@ -34,5 +42,79 @@ const operate = (operator, num1, num2) => {
    };
 
 };
+
+
+function addOperator(operator) {
+   currentDisplay.textContent += operandButtons.textContent
+}
+
+
+function addOperand(operand) {
+   currentDisplay.textContent += operand
+}
+
+
+function clear() {
+   currentDisplay.textContent = '0';
+   topDisplay.textContent = '';
+   firstOperand = '';
+   lastOperand = '';
+   currentOperator = null;
+
+};
+
+function ResetScreen() {
+   currentDisplay.textContent = ''; 
+   doResetScreen = true
+
+}
+
+
+function keyboardPress(e) {
+   if (e.key >= 0 && e.key <= 9) addOperand(e.key) 
+   if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/') setOperator(convertOperator(e.key))
+}
+
+
+function setOperator(operator) {
+   if (currentOperator !== null) evalNumber()
+   firstOperand = currentDisplay.textContent
+   currentOperator = operator
+   topDisplay.textContent = `${firstOperand} ${currentOperator}`
+   resetScreen = true
+}
+
+
+
+function evalNumber() {
+   if (currentOperator === null || resetScreen) {
+      return
+   }
+   secondOperand = currentDisplay.textContent
+   currentDisplay.textContent = roundResult (
+      operate(currentOperator, firstOperand, secondOperand)
+   )
+   topDisplay.textContent = `${firstOperand} ${currentOperator} ${secondOperand}`
+   currentOperator = null
+
+}
+
+
+
+function roundResult(num) {
+   return Math.round(num * 1000) / 1000
+}
+
+
+
+
+function convertOperator(keyOperator) {
+   if (keyOperator === '*') return 'x'
+   if (keyOperator === '/') return '÷'
+   if (keyOperator === '+') return '+'
+   if (keyOperator === '-') return '-'
+}
+
+
 
 
