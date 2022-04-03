2ba3a588ea3bff933ceca8071d704feb9ab3d1b1
Nabiel Alif <nabiel.alif01@gmail.com>
2022-04-04 03:16:21 +0700

    Operate function

--- main.js
+++ main.js
@@ -1,30 +1,72 @@
+let currentNumber = '';
+let previousNumber = '';
+let operator = '';
 
+const currentDisplay = document.querySelector('.current-display');
+const topDisplay = document.querySelector('.top-display');
 
-function add(a, b) {
-   return a + b
+const equalBtn = document.querySelector('.equalBtn');
+const decimalBtn = document.querySelector('.decimalBtn')
+const clearBtn = document.querySelector('.clearBtn')
+const numberBtn = document.querySelectorAll('.operand')
+const operatorBtn = document.querySelectorAll('.operator')
+
+equalBtn.addEventListener('click', operate)
+
+numberBtn.forEach(button => {
+   button.addEventListener('click', (e) => {
+      handleNumber(e.target.textContent)
+   });
+});
+
+function handleNumber(number) {
+   if (currentNumber.length <= 11) {
+      currentNumber += number
+      currentDisplay.textContent = currentNumber
+   }
 }
 
-function subtract(a, b) {
-   return a - b
+operatorBtn.forEach(button => {
+   button.addEventListener('click', (e) => {
+      handleOperator(e.target.textContent)
+   });
+});
+
+function handleOperator(op) {
+   operator = op
+   previousNumber = currentNumber
+   topDisplay.textContent = `${previousNumber} ${operator} `
+   currentNumber = ''
+   currentDisplay.textContent = ''
+
 }
 
-function multiply(a, b) {
-   return a * b
+
+function operate() {
+   previousNumber = Number(previousNumber); // Convert value to number
+   currentNumber = Number(currentNumber); //Convert value to number 
+
+   const add = (a, b) => a + b
+   const subtract = (a, b) => a - b
+   const multiply = (a, b) => a * b
+   const divide = (a, b) => a / b
+
+   if (operator === '+') {
+      previousNumber = add(currentNumber, previousNumber)
+   } else if (operator === '-') {
+      previousNumber = subtract(previousNumber, currentNumber)
+   } else if (operator === 'x') {
+      previousNumber = multiply(currentNumber, previousNumber)
+   } else if (operator === '/' ) {
+      if (currentNumber === 0) {
+         previousNumber = 'ya' 
+      }
+      previousNumber = divide(previousNumber, currentNumber)
+      
+   }
+   previousNumber.toString()
+   currentDisplay.textContent = previousNumber
+   topDisplay.textContent += `${currentNumber}`
+
 }
 
-function divide(a, b) {
-   return a / b
-}
-
-function operate(operator, num1, num2) {
-   num1 = Number(num1);
-   num2 = Number(num2);
-
-   if (operator === '+') return add(num1, num2)
-   if (operator === '-') return subtract(num1, num2)
-   if (operator === '*') return multiply(num1, num2)
-   if (operator === '/') {
-      if (num2 === 0) return null
-      return divide(num1, num2)
-   } 
-}
