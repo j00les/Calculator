674343e1d258227b7fec9760839c739f033c1ef7
Nabiel Alif <nabiel.alif01@gmail.com>
2022-04-05 02:41:00 +0700

    Keyboard press functionality

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// currentDisplay.textContent = currentDisplay.textContent.slice(0, -1)
 let currentNumber = '';
 let previousNumber = '';
 let operator = '';
@@ -8,11 +9,39 @@ const topDisplay = document.querySelector('.top-display');
 const equalBtn = document.querySelector('.equalBtn');
 const decimalBtn = document.querySelector('.decimalBtn');
 const clearBtn = document.querySelector('.clearBtn');
+const allClearBtn = document.querySelector('.allClearBtn')
 const numberBtn = document.querySelectorAll('.operand');
 const operatorBtn = document.querySelectorAll('.operator');
 
-equalBtn.addEventListener('click', operate)
-// clearBtn.addEventListener('click', )
+window.addEventListener('keydown', (e) => {
+   if (e.key >= 0) handleNumber(e.key)
+   if (e.key === '+' || e.key === '-' ||
+       e.key === '/' || e.key === '*') {
+      handleOperator(e.key)
+   }
+   if (e.key === 'Enter') {
+      if (previousNumber.length === 0 || currentNumber.length === 0) {
+         currentDisplay.textContent = NaN
+      } else {
+         operate()
+      }
+   } 
+})
+
+equalBtn.addEventListener('click', () => {
+   if (currentNumber.length === 0 || previousNumber.length === 0) {
+      currentDisplay.textContent = NaN
+   } else {
+      operate()
+   }
+});
+
+clearBtn.addEventListener('click', () => {
+   currentNumber = ''
+   previousNumber = ''
+   topDisplay.textContent = ''
+   currentDisplay.textContent = ''
+})
 
 numberBtn.forEach(button => {
    button.addEventListener('click', (e) => {
@@ -35,7 +64,7 @@ operatorBtn.forEach(button => {
 
 function handleOperator(op) {
    operator = op
-   previousNumber = currentNumber
+   previousNumber += currentNumber
    topDisplay.textContent = `${previousNumber} ${operator} `
    currentNumber = ''
    currentDisplay.textContent = ''
@@ -45,6 +74,7 @@ function operate() {
    previousNumber = Number(previousNumber); // Convert value to number
    currentNumber = Number(currentNumber); //Convert value to number 
 
+   const roundNumber = (num) => Math.round(num * 100000) / 100000
    const add = (a, b) => a + b
    const subtract = (a, b) => a - b
    const multiply = (a, b) => a * b
@@ -63,6 +93,7 @@ function operate() {
       }
    }
    topDisplay.textContent += currentNumber
+   previousNumber = roundNumber(previousNumber)
    previousNumber = previousNumber.toString()
    alterResult()
 }
@@ -77,6 +108,8 @@ function alterResult() {
    if (previousNumber.length <= 11) {
       currentDisplay.textContent = previousNumber
    } else {
-      currentDisplay.textContent = `${previousNumber.slice(0, 11)}`
+      currentDisplay.textContent = `${previousNumber.slice(0, 11)}...`
    }
 }
+
+
