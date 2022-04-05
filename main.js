436503ee0c128fcfe77d5aa046ef64eeaf936761
Nabiel Alif <nabiel.alif01@gmail.com>
2022-04-05 16:12:47 +0700

    Restructuring

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
-// currentDisplay.textContent = currentDisplay.textContent.slice(0, -1)
 let currentNumber = '';
 let previousNumber = '';
 let operator = '';
-
 const currentDisplay = document.querySelector('.current-display');
 const topDisplay = document.querySelector('.top-display');
-
-const equalBtn = document.querySelector('.equalBtn');
 const decimalBtn = document.querySelector('.decimalBtn');
-const clearBtn = document.querySelector('.clearBtn');
 const allClearBtn = document.querySelector('.allClearBtn')
 const numberBtn = document.querySelectorAll('.operand');
 const operatorBtn = document.querySelectorAll('.operator');
+const clearBtn = document.querySelector('.clearBtn');
+const equalBtn = document.querySelector('.equalBtn');
 
 window.addEventListener('keydown', (e) => {
    if (e.key >= 0) handleNumber(e.key)
@@ -19,7 +16,7 @@ window.addEventListener('keydown', (e) => {
       handleOperator(e.key)
    }
    if (e.key === '*') handleOperator(convertMultiply(e.key))
-
+   
    if (e.key === 'Enter') {
       if (previousNumber.length === 0 || currentNumber.length === 0) {
          currentDisplay.textContent = NaN
@@ -30,31 +27,6 @@ window.addEventListener('keydown', (e) => {
    if (e.key === 'Backspace') handleDeleteKey()
 });
 
-function convertMultiply(keyOp) {
-   if (keyOp === '*') return 'x'
-}
-
-function handleDeleteKey() {
-   if (currentNumber !== '') {
-      currentNumber = currentNumber.slice(0, -1)
-      currentDisplay.textContent = currentNumber;
-      if (currentNumber === '') {
-         currentDisplay.textContent = '0'
-      }
-   }
-   if (currentNumber === '' && previousNumber !== '' && operator === '') {
-      previousNumber = previousNumber.slice(0, -1)
-      currentDisplayNumber.textcontent = previousNum;
-   }
-}
-
-equalBtn.addEventListener('click', () => {
-   if (currentNumber.length === 0 || previousNumber.length === 0) {
-      currentDisplay.textContent = NaN
-   } else {
-      operate()
-   }
-});
 
 clearBtn.addEventListener('click', () => {
    currentNumber = ''
@@ -64,18 +36,21 @@ clearBtn.addEventListener('click', () => {
    topDisplay.textContent = ''
 })
 
+equalBtn.addEventListener('click', () => {
+   if (currentNumber.length === 0 || previousNumber.length === 0) {
+      currentDisplay.textContent = NaN
+   } else {
+      operate()
+   }
+});
+
+
 numberBtn.forEach(button => {
    button.addEventListener('click', (e) => {
       handleNumber(e.target.textContent)
    });
 });
 
-function handleNumber(number) {
-   if (currentNumber.length <= 11) {
-      currentNumber += number
-      currentDisplay.textContent = currentNumber
-   }
-}
 
 operatorBtn.forEach(button => {
    button.addEventListener('click', (e) => {
@@ -83,10 +58,11 @@ operatorBtn.forEach(button => {
    });
 });
 
+// All calculation goes here
 function operate() {
    previousNumber = Number(previousNumber); // Convert value to number
    currentNumber = Number(currentNumber); //Convert value to number 
-
+   
    const roundNumber = (num) => Math.round(num * 100000) / 100000
    const add = (a, b) => a + b
    const subtract = (a, b) => a - b
@@ -111,12 +87,14 @@ function operate() {
    alterResult()
 }
 
+// Show error if e.g 9 / 0
 function displayResult() {
    previousNumber = 'Error!'
    topDisplay.textContent = ''
    currentNumber = ''
 }
 
+// Altering result if it's overflowing the display
 function alterResult() {
    if (previousNumber.length <= 11) {
       currentDisplay.textContent = previousNumber
@@ -137,7 +115,7 @@ function handleOperator(op) {
       currentDisplay.textContent = '0';
       topDisplay.textContent = `${previousNum} ${operator}`
    }
-
+   
    // operator = op
    // previousNumber += currentNumber
    // topDisplay.textContent = `${previousNumber} ${operator} `
@@ -152,6 +130,29 @@ function operatorCheck(text) {
    currentNumber = ''
 }
 
+function convertMultiply(keyOp) {
+   if (keyOp === '*') return 'x'
+}
 
+function handleDeleteKey() {
+   if (currentNumber !== '') {
+      currentNumber = currentNumber.slice(0, -1)
+      currentDisplay.textContent = currentNumber;
+      if (currentNumber === '') {
+         currentDisplay.textContent = '0'
+      }
+   }
+   if (currentNumber === '' && previousNumber !== '' && operator === '') {
+      previousNumber = previousNumber.slice(0, -1)
+      currentDisplayNumber.textcontent = previousNum;
+   }
+}
+
+function handleNumber(number) {
+   if (currentNumber.length <= 11) {
+      currentNumber += number
+      currentDisplay.textContent = currentNumber
+   }
+}
 
 
